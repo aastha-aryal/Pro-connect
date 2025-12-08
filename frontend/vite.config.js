d50efbb73aa42823e44bdc3a8fa3d740e28b0d53
aastha-aryal <rucheeta288@gmail.com>
2025-12-08 17:37:25 +0545

    Updated frontend components

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Pro-connect/', //  this line is for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
-
